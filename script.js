b1c63358fc21b035491e581c78f8d7aef7953071
Max Oberberger <max@oberbergers.de>
2014-01-26 19:24:26 +0100

    insert new media, script.js and update colour from gray to blue

---
+++ script.js
@@ -0,0 +1,25 @@
+// Slideshow
+// a = index des Artikels
+// l = Anzahl der Artikel in Slideshow
+var articles = document.querySelectorAll("#overview > article"), a = 0, l = articles.length, slideshow;
+
+// Interval 4000 = alle 4 sec rufe Funktion auf
+slideshow = setInterval(function() {
+    for (i = 0; i < l; i++) {
+        /*if (i == a) {
+            articles[i].style.opacity = 1;
+        } else {
+            articles[i].style.opacity = 0;
+        }*/
+        articles[i].style.opacity = i==a ? 1 : 0;
+    }
+    a++;
+    if (a==l){
+        a=0;
+    }
+}, 4000);
+
+// Nach 5 Minuten Slideshow beenden
+setTimeout(function() {
+    clearInterval(slideshow);
+}, 5*60*1000);
