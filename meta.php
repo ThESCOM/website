d67a5c583cca1a632be9b0c7bb754750b3ed7569
Max Oberberger <max@oberbergers.de>
2014-02-22 09:00:40 +0100

    adapt meta tags

--- meta.php
+++ meta.php
@@ -2,7 +2,7 @@
     <!--<meta http-equiv="Content-Type" content="text/html; charset=UTF8" />-->
     <!-- Bei HTML 5 nur noch reduziert auf: -->
     <meta charset="UTF8" />
-    <meta name="keywords" content="Max Oberberger, IT Solution, Apps, Programmierung, Websites, Hosting, Traunstein, Bayern, Informatik, Computer Probleme"/>
+    <meta name="keywords" content="Max Oberberger, IT Solution, Apps, Programmierung, Websites, Hosting, Traunstein, Bayern, Informatik, Computer Probleme, Oberberger, Git, vcs, continuous integration, edv, homepage, administration, Administrator, Verschlüsselung, encryption, Linux, Unix"/>
     <meta name="author" content="Max Oberberger"/>
     <meta name="copyright" content="Max Oberberger"/>
     <meta name="publisher" content="Max Oberberger"/>
