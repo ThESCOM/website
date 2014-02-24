1a1a17d3cf11a2d160bc377017665edc39f3c4b1
Max Oberberger <max@oberbergers.de>
2014-02-24 18:35:16 +0100

    insert project history to ueber-mich and adapt css style for that

--- ueber-mich.php
+++ ueber-mich.php
@@ -14,21 +14,57 @@
             <div id="info-box-horizontal-description">
                 <p><mark>Name:</mark> Max Oberberger</p>
                 <p><mark>Abschluss:</mark> Bachelor of Science (FH-Rosenheim)</p>
+                <p><mark>Geburtsdatum:</mark> 10.02.1988</p>
                 <h3>Projekthistorie</h3>
-                <table>
-                    <thead>
-                        <tr>
-                        <th>Zeitraum</th>
-                        <th>Tätigkeit</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <td>2013 - 2014</td>
-                        <td>Zu tun</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <ul>
+                    <li><mark>09/13 - 11/13 (Elektroniksystem- und Logistik-GmbH)</mark>
+                        <ul>
+                            <li>Systemadministrator SuSE</li>
+                            <li>Client-Migration von SuSE auf Ubuntu</li>
+                            <li>Git-Schulung für das Entwicklerteam</li>
+                            <li>Client-Konfiguration mit Puppet</li>
+                            <li>Qt-Gui Entwicklung mit C++</li>
+                        </ul>
+                    </li>
+                    <br />
+                    <li><mark>03/13 - 08/13 (Linux Information Systems AG)</mark>
+                        <ul>
+                            <li>Webentwicklugn mit Django</li>
+                            <li>Firefox-Plugin Entwicklung</li>
+                            <li>OPSI-Paketierung</li>
+                            <li>Client-/Serverkonfiguration mit Cfengine2</li>
+                        </ul>
+                    </li>
+                    <br />
+                    <li><mark>10/12 - 02/13 (FH-Rosenheim)</mark>
+                        <ul>
+                            <li>Wetterstation auf Android-Basis mit externer Server Anbindung und Android-Widget zur Sensor-Darstellung</li>
+                        </ul>
+                    </li>
+                    <br />
+                    <li><mark>03/12 - 07/12 (FH-Rosenheim)</mark>
+                        <ul>
+                            <li>Android App (erideaResApp) für das reservieren, buchen und belegen von Stromtankstellen im Auftrag der Eridea</li>
+                        </ul>
+                    </li>
+                    <br />
+                    <li><mark>10/11 - 02/12 (Linux Information Systems AG)</mark>
+                        <ul>
+                            <li>Systemadministration Ubuntu Server</li>
+                            <li>Entwicklung mit Python und Perl</li>
+                            <li>Paketierung von deb-Paketen</li>
+                            <li>Umstellung des Entwicklerworkflows von SVN auf Git</li>
+                            <li>Client-/Serverkonfiguration mit Cfengine2</li>
+                        </ul>
+                    </li>
+                    <br />
+                    <li><mark>08/10 - 09/11 (Elektroniksystem- und Logistik-GmbH)</mark>
+                        <ul>
+                            <li>Systemadministrator SuSE</li>
+                            <li>Paketierung von rpm-Paketen</li>
+                            <li>Integration und Programmierung des Steuerknüppels für einen Kampfjetsimulator (Programmiersprachen: C, C++ -- Hardwarenahe Programmierung)</li>
+                        </ul>
+                    </li>
             </div>
             <div>
                 <img src="media/images/portrait.jpg">
