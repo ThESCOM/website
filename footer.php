595f674424a12c3fb624bea74085e507d408b608
Max Oberberger <max@oberbergers.de>
2014-02-24 17:36:30 +0100

    Revert "deny screenshot"
    This reverts commit 65640db75ba93588b22f4bbcc27a64f0b104528f.

--- footer.php
+++ footer.php
@@ -5,18 +5,3 @@
     <img id="googleplus" src="media/images/google-plus.png">
     </p>
 </footer>
-<!-- präsentiert von kostenlose-javascripts.de -->
-<script type='text/javascript'>
-<!--
-function ccb(){ 
-    if(clipboardData){ 
-        clipboardData.clearData(); 
-    } 
-} 
-window.onerror=new Function("return true;"); 
-setInterval("ccb();", 1000);
-//-->
-</script>
-<!-- präsentiert von kostenlose-javascripts.de --><br />
-
-<br />
