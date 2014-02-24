f7e3d386be5c9fe0dcfbb02244ab9cd819263451
Max Oberberger <max@oberbergers.de>
2014-02-24 17:49:01 +0100

    insert google+ url to footer.php

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
     <p>&copy; 2013 ThESCOM (Max Oberberger) | <a href="impressum.php">Impressum</a>
     <a href="https://www.facebook.com/thescomIt"><img id="facebook" src="media/images/facebook.png"></a>
     <a href="https://twitter.com/chiemseesurfer"><img id="twitter" src="media/images/twitter.png"></a>
-    <img id="googleplus" src="media/images/google-plus.png">
+    <a href="https://plus.google.com/+ThescomDeTS"><img id="googleplus" src="media/images/google-plus.png"></a>
     </p>
 </footer>
