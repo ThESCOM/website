7f62f0c147dfed177252272b1f25c1fc4038ab6b
Max Oberberger <max@oberbergers.de>
2014-02-25 14:44:43 +0100

    change border-color from gray to blue and adapt copyright information in footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>&copy; 2013 ThESCOM (Max Oberberger) | <a href="impressum.php">Impressum</a>
+    <p>&copy; 2013 - 2014 ThESCOM (Max Oberberger) | <a href="impressum.php">Impressum</a>
     <a href="https://www.facebook.com/thescomIt"><img id="facebook" src="media/images/Facebook.png"></a>
     <a href="https://twitter.com/chiemseesurfer"><img id="twitter" src="media/images/Twitter.png"></a>
     <a href="https://google.com/+ThescomDeTS"><img id="googleplus" src="media/images/Google plus.png"></a>
