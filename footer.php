5d7722106dfe5ad54fe57cde2e751823e00985d3
Max Oberberger <max@oberbergers.de>
2014-02-25 12:14:41 +0100

    adapt footer - social media icons

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
     <p>&copy; 2013 ThESCOM (Max Oberberger) | <a href="impressum.php">Impressum</a>
     <a href="https://www.facebook.com/thescomIt"><img id="facebook" src="media/images/facebook.png"></a>
     <a href="https://twitter.com/chiemseesurfer"><img id="twitter" src="media/images/twitter.png"></a>
-    <a href="https://google.com/+ThescomDeTS"><img id="googleplus" src="media/images/google-plus.png"></a>
+    <a href="https://google.com/+ThescomDeTS"><img id="googleplus" src="media/images/googleplus.png"></a>
     </p>
 </footer>
