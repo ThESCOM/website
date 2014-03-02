18f6c4740e25d9c6d192d9eabf0e6eb37468172e
Max Oberberger <max@oberbergers.de>
2014-03-02 01:02:11 +0100

    open social network links on new tab. Make ThESCOM-Logo to link to Startpage

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
     <p>&copy; 2013 - 2014 ThESCOM (Max Oberberger) | <a href="impressum.php">Impressum</a>
-    <a href="https://www.facebook.com/thescomIt"><img id="facebook" src="media/images/Facebook.png"></a>
-    <a href="https://twitter.com/chiemseesurfer"><img id="twitter" src="media/images/Twitter.png"></a>
-    <a href="https://google.com/+ThescomDeTS"><img id="googleplus" src="media/images/Google plus.png"></a>
+    <a href="https://www.facebook.com/thescomIt" target="_blank"><img id="facebook" src="media/images/Facebook.png"></a>
+    <a href="https://twitter.com/chiemseesurfer" target="_blank"><img id="twitter" src="media/images/Twitter.png"></a>
+    <a href="https://google.com/+ThescomDeTS" target="_blank"><img id="googleplus" src="media/images/Google plus.png"></a>
     </p>
 </footer>
