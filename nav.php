502dcdd4e1a49df06390805099392a7e78d7b52e
Max Oberberger <max@oberbergers.de>
2014-03-02 13:01:47 +0100

    adapt nav style to disable light-blue border

--- nav.php
+++ nav.php
@@ -1,8 +1,8 @@
 <nav>
     <ul>
-        <li style="background-color: #03426b; color: white;"><a href="index.php">Startseite</a></li>
+        <li style="border: 1px solid #03426b; background-color: #03426b; color: white;"><a href="index.php">Startseite</a></li>
         <li><a href="ueber-mich.php">Über mich</a></li>
-        <li style="background-color: #0ccb03;"><a href="service.php">Service</a></li>
+        <li style="border: 1px solid #0ccb03; background-color: #0ccb03;"><a href="service.php">Service</a></li>
         <li><a href="referenzen.php">Referenzen</a></li>
         <li></li>
     </ul>
