7b23614201cce6ef4223aaec809c9aef4d13dabf
Max Oberberger <max@oberbergers.de>
2014-03-06 19:17:38 +0100

    adapt nav and cleanup style.css

--- nav.php
+++ nav.php
@@ -1,10 +1,10 @@
 <nav>
     <ul>
-        <li style="border: 1px solid #03426b; background-color: #03426b; color: white;"><a href="index.php">Startseite</a></li>
-        <li><a href="ueber-mich.php">Über mich</a></li>
-        <li style="border: 1px solid #0ccb03; background-color: #0ccb03;"><a href="service.php">Service</a></li>
-        <li style="margin-left:-4px;background:dark;border: 1px solid #03426b; background-color: #03426b; color: white;"><a href="referenzen.php">Referenzen</a></li>
-        <li style="margin-left:-4px;"><a href="kontakt.php">Kontakt</a></li>
+        <li id="nav-dark-blue"><a href="index.php">Startseite</a></li>
+        <li id="nav-light-blue"><a href="ueber-mich.php">Über mich</a></li>
+        <li id="nav-green"><a href="service.php">Service</a></li>
+        <li id="nav-dark-blue"><a href="referenzen.php">Referenzen</a></li>
+        <li id="nav-light-blue"><a href="kontakt.php">Kontakt</a></li>
         <li></li>
     </ul>
 </nav>
