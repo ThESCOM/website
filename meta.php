cd556417d99d5ffd02cc038d3cda4f5d54e2c7ed
Max Oberberger <max@oberbergers.de>
2014-11-14 15:32:38 +0100

    adapt unicorn errors

--- meta.php
+++ meta.php
@@ -1,11 +1,11 @@
 <head>
     <!--<meta http-equiv="Content-Type" content="text/html; charset=UTF8" />-->
     <!-- Bei HTML 5 nur noch reduziert auf: -->
-    <meta charset="UTF8" />
+    <meta charset="utf-8" />
     <meta name="keywords" content="Max Oberberger, IT Solution, Apps, Programmierung, Websites, Hosting, Traunstein, Bayern, Informatik, Computer Probleme, Oberberger, Git, vcs, continuous integration, edv, homepage, administration, Administrator, Verschlüsselung, encryption, Linux, Unix"/>
     <meta name="author" content="Max Oberberger"/>
-    <meta name="copyright" content="Max Oberberger"/>
-    <meta name="publisher" content="Max Oberberger"/>
+    <!--<meta name="copyright" content="Max Oberberger"/>
+    <meta name="publisher" content="Max Oberberger"/>-->
     <meta name="description" content="Freiberufler Max Oberberger hilft Ihnen bei all Ihren Computerproblemen"/>
     <meta http-equiv="imagetoolbar" content="no">
 
