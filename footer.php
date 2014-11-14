36ed62085f89ad5b8e24d621c9e9e558a2d1de0f
Max Oberberger <max@oberbergers.de>
2014-11-14 15:35:15 +0100

    add alt attribute to img attributes

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer>
     <p>&copy; 2013 - 2014 ThESCOM (Max Oberberger) | <a href="impressum.php">Impressum</a> | <a href="datenschutz.php">Datenschutz</a>
-    <a href="https://www.facebook.com/thescomIt" target="_blank"><img id="facebook" src="media/images/Facebook.png"></a>
-    <a href="https://twitter.com/chiemseesurfer" target="_blank"><img id="twitter" src="media/images/Twitter.png"></a>
-    <a href="https://google.com/+ThescomDeTS" target="_blank"><img id="googleplus" src="media/images/Google plus.png"></a>
+    <a href="https://www.facebook.com/thescomIt" target="_blank"><img alt="facebook-icon" id="facebook" src="media/images/Facebook.png"></a>
+    <a href="https://twitter.com/chiemseesurfer" target="_blank"><img alt="twitter-icon" id="twitter" src="media/images/Twitter.png"></a>
+    <a href="https://google.com/+ThescomDeTS" target="_blank"><img alt="googleplus-icon" id="googleplus" src="media/images/Google plus.png"></a>
     </p>
 </footer>
 
--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <hgroup>
         <!--<img src="media/images/logo.png" height="100">-->
     </hgroup>
-    <a style="position: absolute;" href="index.php"><img src="media/images/transparent.png" height="100" width="200"></a>
+    <a style="position: absolute;" href="index.php"><img alt="transparent overflow" src="media/images/transparent.png" height="100" width="200"></a>
     <div class="description">
         <p><span style="color: #03426b;">Thoroughly Engineering Solutions &</span> <span style="color: #1eadfe;">Consulting</span> <span style="color: #0ccb03;">Oberberger
         Max</span></p>
