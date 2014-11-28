30616ff95c6d82317decef5f3719b3928f37cbe0
Max Oberberger <max@oberbergers.de>
2014-11-28 10:32:23 +0100

    remove duplicate nav ids

--- nav.php
+++ nav.php
@@ -1,10 +1,10 @@
 <nav>
     <ul>
-        <li id="nav-dark-blue"><a href="index.php">Startseite</a></li>
-        <li id="nav-light-blue"><a href="ueber-mich.php">Über mich</a></li>
+        <li id="nav-dark-blue-start"><a href="index.php">Startseite</a></li>
+        <li id="nav-light-blue-me"><a href="ueber-mich.php">Über mich</a></li>
         <li id="nav-green"><a href="service.php">Service</a></li>
-        <li id="nav-dark-blue"><a href="referenzen.php">Referenzen</a></li>
-        <li id="nav-light-blue"><a href="kontakt.php">Kontakt</a></li>
+        <li id="nav-dark-blue-ref"><a href="referenzen.php">Referenzen</a></li>
+        <li id="nav-light-blue-kont"><a href="kontakt.php">Kontakt</a></li>
         <li></li>
     </ul>
 </nav>
