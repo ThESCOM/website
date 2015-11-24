f031717d296d412ca6160ff0631e3a46d9e57c8b
Max Oberberger <max@oberbergers.de>
2015-11-24 10:00:07 +0100

    add cookie notice to meta part

--- meta.php
+++ meta.php
@@ -12,6 +12,16 @@
     <link rel="shortcut icon" href="/media/images/favicon.ico">
 
     <title>ThESCOM - Thoroughly Engineering Solutions & Consulting Oberberger Max</title>
+    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+    <!-- Paragraph 15 Abs.3 Telemediengesetz (TMG)
+         http://www.e-recht24.de/artikel/datenschutz/8451-hinweispflicht-fuer-cookies.html
+    --> 
+    <script type="text/javascript">
+        window.cookieconsent_options = {"message":"Wir verwenden Cookies von Piwik, um Ihre Zugriffe zu analysieren. Informationen darüber, wie Sie die Website verwenden, werden an Piwik weitergeleitet. Durch die Nutzung dieser Website erklären Sie sich damit einverstanden, dass Sie Cookies verwendet.","dismiss":"Verstanden!","learnMore":"Genauere Informationen","link": "http://www.thescom.de/datenschutz.php","theme":"dark-top"};
+    </script>
+
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
+    <!-- End Cookie Consent plugin -->
 
     <!--<link href="media/css/style.css" rel="stylesheet" type="text/css"/>-->
 <script>
